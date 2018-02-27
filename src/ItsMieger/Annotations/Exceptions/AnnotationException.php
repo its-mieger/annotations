b6868aa09dcb1027195052e39e51e4bcbf53f9eb
chris <chris@chris-ThinkPad-W520>
2018-02-27 21:01:21 +0100

    Added annotation exception class

---
+++ src/ItsMieger/Annotations/Exceptions/AnnotationException.php
@@ -0,0 +1,13 @@
+<?php
+
+	namespace ItsMieger\Annotations\Exceptions;
+
+
+	/**
+	 * Thrown if annotations are not defined as expected
+	 * @package ItsMieger\Annotations\Exceptions
+	 */
+	class AnnotationException extends \Exception
+	{
+
+	}
